118d667f722e24ecb7bf748fa5f048fac3519a44
jason13official <jason13official@gmail.com>
2026-05-11 00:42:15 -0400

    formatting

--- common/src/main/java/com/example/examplemod/mixin/MixinMinecraft.java
+++ common/src/main/java/com/example/examplemod/mixin/MixinMinecraft.java
@@ -1,9 +1,7 @@
 package com.example.examplemod.mixin;
 
 import com.example.examplemod.Constants;
-import com.example.examplemod.platform.Services;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +9,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-    
-    @Inject(at = @At("TAIL"), method = "<init>")
-    private void init(CallbackInfo info) {
 
-        Constants.LOG.info("This line is printed by an example mod common mixin!");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-    }
+  @Inject(at = @At("TAIL"), method = "<init>")
+  private void init(CallbackInfo info) {
+
+    Constants.LOG.info("This line is printed by an example mod common mixin!");
+    Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+  }
 }
