efd45f1e9539b3a4507d3595df933b967ad7ba2f
jason13official <jason13official@gmail.com>
2026-05-11 00:18:01 -0400

    mixin and services explanation

--- common/src/main/java/com/example/examplemod/mixin/MixinMinecraft.java
+++ common/src/main/java/com/example/examplemod/mixin/MixinMinecraft.java
@@ -1,6 +1,7 @@
 package com.example.examplemod.mixin;
 
 import com.example.examplemod.Constants;
+import com.example.examplemod.platform.Services;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +13,8 @@ public class MixinMinecraft {
     
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
-        
+
+        Constants.LOG.info(Services.PLATFORM.getPlatformName());
         Constants.LOG.info("This line is printed by an example mod common mixin!");
         Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
